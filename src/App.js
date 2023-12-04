5fdd31345f7dd65a3b160d2d02eb5825c4715146
Helen Liu <helenliu@vl965-172-31-68-253.wireless.umass.edu>
2023-12-04 06:36:14 -0500

    Home page not empty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' exact element={<Home/>}></Route>
       </Routes>
     </Router>
     </>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeroSection.css';
+import '../App.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <h1>Hello!</h1>
+        <p>I'm Helen Liu.</p>
+    </div>
+  )
+}
+
+export default HeroSection;
